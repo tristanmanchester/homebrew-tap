720a372cc86e0145569741d422b5c2cac1a1fea4
Tristan Manchester <tmanchester96@gmail.com>
2026-04-17 08:16:12 +0200

    fix: remove redundant formula version

--- Formula/clipmem.rb
+++ Formula/clipmem.rb
@@ -1,7 +1,6 @@
 class Clipmem < Formula
   desc "macOS clipboard memory backed by SQLite and searchable from OpenClaw"
   homepage "https://github.com/tristanmanchester/clipmem"
-  version "0.1.0"
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/tristanmanchester/clipmem/releases/download/v0.1.0/clipmem-aarch64-apple-darwin.tar.xz"
     sha256 "b66894053d53ebd1df11dfe39fe2af4c6011b11bd363a26f8f2d3520e113111e"
